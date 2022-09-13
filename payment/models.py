df09cd795293bc6a86b49982a6cde6e4c9e79c6f
cheezedoodles <hougvapes@gmail.com>
2022-09-13 18:34:30 +0300

    added Item model

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,16 @@
+from unicodedata import decimal
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=128, db_index=True)
+    description = models.TextField()
+    # using bigint in prod. instead of decimal when working 
+    # with currencies is, as i know, a better practice
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        ordering = ('name',)
+    
+    def __str__(self):
+        return self.name
